cbcf17233dda85b2bfefb83d17ac9222c37de245
Jungsoo Kim <jungsoo@mit.edu>
2020-02-06 16:22:29 -0500

    splitting rof kernels

--- src/caller.jl
+++ src/caller.jl
@@ -2,8 +2,10 @@ function gpu_imROF(img::Array{Float32,2}, λ, maxitr)
     # GPU version of imROF of Images package
     size_x, size_y = size(img)
 
-    block_n_x = Int(ceil(size_x / 16))
-    block_n_y = Int(ceil(size_y / 16))
+    n_th = 16 # number of thread
+
+    b_n_x = Int(ceil(size_x / n_th)) # blocks x
+    b_n_y = Int(ceil(size_y / n_th)) # blocks y
 
     d_img = CuArray(img)
     d_p = CuArray(zeros(Float32, size_x, size_y, 2))
@@ -16,7 +18,42 @@ function gpu_imROF(img::Array{Float32,2}, λ, maxitr)
 
     # This iterates Eq. (9) of the Chambolle citation
     for k = 1:maxitr
-        @cuda threads=(16,16) blocks=(block_n_x,block_n_y) kernel_rof(d_p, d_p_div, d_u, d_grad_u, d_grad_u_mag, d_img, λ, τ, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_div(d_p_div, d_p, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_u(d_u, d_img, λ, d_p_div, size_x, size_y) # multiply term inside ∇ by -λ. Thm. 3.1 relates this to u via Eq. 7.
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_grad_u(d_grad_u, d_u, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_grad_u_mag(d_grad_u_mag, d_grad_u, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_p_update(d_p, d_grad_u, d_grad_u_mag, λ, τ, size_x, size_y)
     end
+
+    Array(d_u)
+end
+
+function gpu_imROF(img::Array{Float32,2}, λ, maxitr)
+    # GPU version of imROF of Images package
+    size_x, size_y = size(img)
+
+    n_th = 16 # number of thread
+
+    b_n_x = Int(ceil(size_x / n_th)) # blocks x
+    b_n_y = Int(ceil(size_y / n_th)) # blocks y
+
+    d_img = CuArray(img)
+    d_p = CuArray(zeros(Float32, size_x, size_y, 2))
+    d_p_div = CuArray(zeros(Float32, size_x, size_y))
+    d_u = CuArray(zeros(Float32, size_x, size_y))
+    d_grad_u = CuArray(zeros(Float32, size_x, size_y, 2))
+    d_grad_u_mag = CuArray(zeros(Float32, size_x, size_y))
+
+    τ = 0.25 # see 2nd remark after proof of Theorem 3.1.
+
+    # This iterates Eq. (9) of the Chambolle citation
+    for k = 1:maxitr
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_div(d_p_div, d_p, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_u(d_u, d_img, λ, d_p_div, size_x, size_y) # multiply term inside ∇ by -λ. Thm. 3.1 relates this to u via Eq. 7.
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_grad_u(d_grad_u, d_u, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_grad_u_mag(d_grad_u_mag, d_grad_u, size_x, size_y)
+        @cuda threads=(n_th,n_th) blocks=(b_n_x,b_n_y) kernel_p_update(d_p, d_grad_u, d_grad_u_mag, λ, τ, size_x, size_y)
+    end
+
     Array(d_u)
 end
